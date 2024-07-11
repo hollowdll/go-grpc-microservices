df3d926708242480f099cd04325ff8861964bf78
hollowdll <pixerchat@gmail.com>
2024-07-11 15:43:03 +0300

    add payment status type

--- services/payment/internal/application/core/domain/payment.go
+++ services/payment/internal/application/core/domain/payment.go
@@ -6,21 +6,34 @@ import (
 	"github.com/google/uuid"
 )
 
+type PaymentStatus string
+
+const (
+	PaymentPending   PaymentStatus = "Pending"
+	PaymentCompleted PaymentStatus = "Completed"
+	PaymentFailed    PaymentStatus = "Failed"
+	PaymentCancelled PaymentStatus = "Cancelled"
+)
+
+func (s PaymentStatus) String() string {
+	return string(s)
+}
+
 type Payment struct {
-	ID              string `json:"id"`
-	CustomerID      string `json:"customer_id"`
-	Status          string `json:"status"`
-	OrderID         string `json:"order_id"`
-	TotalPriceCents int32  `json:"total_price_cents"`
-	CreatedAtMillis int64  `json:"created_at_millis"`
-	UpdatedAtMillis int64  `json:"updated_at_millis"`
+	ID              string        `json:"id"`
+	CustomerID      string        `json:"customer_id"`
+	Status          PaymentStatus `json:"status"`
+	OrderID         string        `json:"order_id"`
+	TotalPriceCents int32         `json:"total_price_cents"`
+	CreatedAtMillis int64         `json:"created_at_millis"`
+	UpdatedAtMillis int64         `json:"updated_at_millis"`
 }
 
 func NewPayment(customerID string, orderID string, totalPriceCents int32) *Payment {
 	return &Payment{
 		ID:              uuid.NewString(),
 		CustomerID:      customerID,
-		Status:          "Pending",
+		Status:          PaymentPending,
 		OrderID:         orderID,
 		TotalPriceCents: totalPriceCents,
 		CreatedAtMillis: time.Now().UnixMilli(),
