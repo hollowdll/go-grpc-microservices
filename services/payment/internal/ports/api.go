0775acef89314623cf82acc142197e1db37ae203
hollowdll <pixerchat@gmail.com>
2024-07-12 11:24:22 +0300

    add payment APIPort interface

---
+++ services/payment/internal/ports/api.go
@@ -0,0 +1,13 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/hollowdll/go-grpc-microservices/services/payment/internal/application/core/domain"
+)
+
+// APIPort is an inbound port interface for the core business logic
+// that the application can implement.
+type APIPort interface {
+	Charge(ctx context.Context, payment domain.Payment) (domain.Payment, error)
+}
