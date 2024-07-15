a51429bdfa3da39cd012637a1145e656cde715de
hollowdll <pixerchat@gmail.com>
2024-07-15 12:40:13 +0300

    add run method to payment gRPC adapter

--- services/payment/internal/adapters/grpc/server.go
+++ services/payment/internal/adapters/grpc/server.go
@@ -1,6 +1,10 @@
 package grpc
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/hollowdll/go-grpc-microservices/services/payment/internal/ports"
 	"github.com/hollowdll/grpc-microservices-proto/gen/golang/paymentpb"
 	"google.golang.org/grpc"
@@ -15,4 +19,24 @@ type Adapter struct {
 
 func NewAdapter(api ports.APIPort, port int) *Adapter {
 	return &Adapter{api: api, port: port}
+}
+
+// Run runs the gRPC server and starts to listen for requests.
+func (a *Adapter) Run() {
+	log.Printf("initializing payment service gRPC server on port %d ...", a.port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		log.Fatalf("failed to listen on port %d: %v", a.port, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	paymentpb.RegisterPaymentServiceServer(grpcServer, a)
+
+	log.Printf("starting payment service gRPC server ...")
+	log.Printf("payment service gRPC server listening at %v", lis.Addr())
+
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC at %v", lis.Addr())
+	}
 }
