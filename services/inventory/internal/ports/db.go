2bcfded92a9c3b6a48eae1ec2bdab8156f15736f
hollowdll <pixerchat@gmail.com>
2024-07-18 14:52:42 +0300

    add inventory service ports

---
+++ services/inventory/internal/ports/db.go
@@ -0,0 +1,13 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/hollowdll/go-grpc-microservices/services/inventory/internal/application/core/domain"
+)
+
+type DBPort interface {
+	GetProductDetails(ctx context.Context, productCodes []string) ([]*domain.Product, error)
+	CheckProductStockQuantity(ctx context.Context, products []*domain.ProductQuantity) ([]*domain.ProductStock, error)
+	ReduceProductStockQuantity(ctx context.Context, products []*domain.ProductQuantity) ([]*domain.ProductStock, error)
+}
