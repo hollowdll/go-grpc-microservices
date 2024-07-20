41848571856b64db307671c51e1699f2cceefd68
hollowdll <pixerchat@gmail.com>
2024-07-20 16:03:38 +0300

    implement CheckProductStockQuantity RPC

--- services/inventory/internal/adapters/grpc/grpc.go
+++ services/inventory/internal/adapters/grpc/grpc.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/hollowdll/go-grpc-microservices/services/inventory/internal/application/core/domain"
 	"github.com/hollowdll/grpc-microservices-proto/gen/golang/inventorypb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -26,13 +27,13 @@ func (a *Adapter) GetProductDetails(ctx context.Context, req *inventorypb.GetPro
 		}
 	}()
 
-	products, err := a.api.GetProductDetails(ctx, req.ProductCodes)
+	results, err := a.api.GetProductDetails(ctx, req.ProductCodes)
 	if err != nil {
 		return nil, status.Error(codes.Internal, fmt.Sprintf("failed to get product details: %v", err))
 	}
 
 	var productDetails = []*inventorypb.ProductDetails{}
-	for _, product := range products {
+	for _, product := range results {
 		productDetails = append(productDetails, &inventorypb.ProductDetails{
 			ProductCode:    product.ProductCode,
 			Name:           product.Name,
@@ -42,4 +43,35 @@ func (a *Adapter) GetProductDetails(ctx context.Context, req *inventorypb.GetPro
 	}
 
 	return &inventorypb.GetProductDetailsResponse{ProductDetails: productDetails}, nil
+}
+
+func (a *Adapter) CheckProductStockQuantity(ctx context.Context, req *inventorypb.CheckProductStockQuantityRequest) (res *inventorypb.CheckProductStockQuantityResponse, err error) {
+	log.Printf("call RPC %s: request = %v", checkProductStockQuantityPrefix, req)
+	defer func() {
+		if err != nil {
+			log.Printf("RPC %s failed: request = %v; error = %v", checkProductStockQuantityPrefix, req, err)
+		} else {
+			log.Printf("RPC %s success: request = %v; response = %v", checkProductStockQuantityPrefix, req, res)
+		}
+	}()
+
+	var productQuantities = []*domain.ProductQuantity{}
+	for _, product := range req.Products {
+		productQuantities = append(productQuantities, &domain.ProductQuantity{
+			ProductCode: product.ProductCode,
+			Quantity:    product.Quantity,
+		})
+	}
+	results, err := a.api.CheckProductStockQuantity(ctx, productQuantities)
+
+	var productStocks = []*inventorypb.ProductStock{}
+	for _, result := range results {
+		productStocks = append(productStocks, &inventorypb.ProductStock{
+			ProductCode:       result.ProductCode,
+			AvailableQuantity: result.AvailableQuantity,
+			IsAvailable:       result.IsAvailable,
+		})
+	}
+
+	return &inventorypb.CheckProductStockQuantityResponse{Products: productStocks}, nil
 }
