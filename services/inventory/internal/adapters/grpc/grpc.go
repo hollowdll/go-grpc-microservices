7e4f80c2eab7302d3dbe5566804f6900d9eebcc7
hollowdll <pixerchat@gmail.com>
2024-07-20 16:07:58 +0300

    implement ReduceProductStockQuantity RPC

--- services/inventory/internal/adapters/grpc/grpc.go
+++ services/inventory/internal/adapters/grpc/grpc.go
@@ -75,3 +75,34 @@ func (a *Adapter) CheckProductStockQuantity(ctx context.Context, req *inventoryp
 
 	return &inventorypb.CheckProductStockQuantityResponse{Products: productStocks}, nil
 }
+
+func (a *Adapter) ReduceProductStockQuantity(ctx context.Context, req *inventorypb.ReduceProductStockQuantityRequest) (res *inventorypb.ReduceProductStockQuantityResponse, err error) {
+	log.Printf("call RPC %s: request = %v", reduceProductStockQuantityPrefix, req)
+	defer func() {
+		if err != nil {
+			log.Printf("RPC %s failed: request = %v; error = %v", reduceProductStockQuantityPrefix, req, err)
+		} else {
+			log.Printf("RPC %s success: request = %v; response = %v", reduceProductStockQuantityPrefix, req, res)
+		}
+	}()
+
+	var productQuantities = []*domain.ProductQuantity{}
+	for _, product := range req.Products {
+		productQuantities = append(productQuantities, &domain.ProductQuantity{
+			ProductCode: product.ProductCode,
+			Quantity:    product.Quantity,
+		})
+	}
+	results, err := a.api.ReduceProductStockQuantity(ctx, productQuantities)
+
+	var productStocks = []*inventorypb.ProductStock{}
+	for _, result := range results {
+		productStocks = append(productStocks, &inventorypb.ProductStock{
+			ProductCode:       result.ProductCode,
+			AvailableQuantity: result.AvailableQuantity,
+			IsAvailable:       result.IsAvailable,
+		})
+	}
+
+	return &inventorypb.ReduceProductStockQuantityResponse{Products: productStocks}, nil
+}
