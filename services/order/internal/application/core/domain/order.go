a513ac64e701b5ef488381c00c4c78eaadfe1ff6
hollowdll <pixerchat@gmail.com>
2024-07-22 15:06:02 +0300

    add order domain objects

---
+++ services/order/internal/application/core/domain/order.go
@@ -0,0 +1,46 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type OrderStatus string
+
+const (
+	OrderPending   OrderStatus = "Pending"
+	OrderCompleted OrderStatus = "Completed"
+	OrderFailed    OrderStatus = "Failed"
+	OrderCancelled OrderStatus = "Cancelled"
+)
+
+type OrderItem struct {
+	ProductCode string `json:"product_code"`
+	Quantity    int32  `json:"quantity"`
+}
+
+type Order struct {
+	ID              string      `json:"id"`
+	CustomerID      string      `json:"customer_id"`
+	Status          OrderStatus `json:"status"`
+	OrderItems      []OrderItem `json:"order_items"`
+	CreatedAtMillis int64       `json:"created_at_millis"`
+	UpdatedAtMillis int64       `json:"updated_at_millis"`
+}
+
+func NewOrder(customerID string, orderItems []OrderItem) (*Order, error) {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Order{
+		ID:              id.String(),
+		CustomerID:      customerID,
+		Status:          OrderPending,
+		OrderItems:      orderItems,
+		CreatedAtMillis: time.Now().UnixMilli(),
+		UpdatedAtMillis: time.Now().UnixMilli(),
+	}, nil
+}
