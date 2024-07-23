9eb21d25a7f058a3d902cc4a9528a0f5cbe55bb6
hollowdll <pixerchat@gmail.com>
2024-07-23 15:20:35 +0300

    init order service payment client

--- services/order/internal/adapters/payment/payment.go
+++ services/order/internal/adapters/payment/payment.go
@@ -1,37 +1,37 @@
-package inventory
+package payment
 
 import (
 	"fmt"
 	"log"
 
 	"github.com/hollowdll/go-grpc-microservices/services/order/config"
-	"github.com/hollowdll/grpc-microservices-proto/gen/golang/inventorypb"
+	"github.com/hollowdll/grpc-microservices-proto/gen/golang/paymentpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Adapter struct {
-	inventory inventorypb.InventoryServiceClient
-	conn      *grpc.ClientConn
+	payment paymentpb.PaymentServiceClient
+	conn    *grpc.ClientConn
 }
 
 func NewAdapter(cfg *config.Config) (*Adapter, error) {
-	log.Printf("creating gRPC client for inventory service ...")
+	log.Printf("creating gRPC client for payment service ...")
 
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	address := fmt.Sprintf("%s:%d", cfg.InventoryServiceHost, cfg.InventoryServicePort)
+	address := fmt.Sprintf("%s:%d", cfg.PaymentServiceHost, cfg.PaymentServicePort)
 
 	log.Printf("using endpoint %s", address)
 	conn, err := grpc.NewClient(address, opts...)
 	if err != nil {
 		return nil, err
 	}
-	client := inventorypb.NewInventoryServiceClient(conn)
+	client := paymentpb.NewPaymentServiceClient(conn)
 
 	return &Adapter{
-		inventory: client,
-		conn:      conn,
+		payment: client,
+		conn:    conn,
 	}, nil
 }
 
